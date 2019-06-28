85036ed87ea28dc82df7773fd95b3ea7e0f42b0f
Brian Bixby <brianbixby0@gmail.com>
2019-06-28 11:15:53 -0700

    push

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.static(`${__dirname}/dist`));
 
+app.get('/*', function(req, res) {   
+  res.sendFile(path.join(__dirname, '/dist/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+});
+
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
