24e3087d79a3340813fb4816276359a4fffdf2aa
Brian Bixby <brianbixby0@gmail.com>
2019-07-04 13:33:14 -0700

    updated api key

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,3 +1,2 @@
 export const renderIf = (condition, component) => condition ? component : undefined;
 export const classToggler = options => Object.keys(options).filter(key => !!options[key]).join(' ');
-export const logError = (...args) => console.error(...args);
